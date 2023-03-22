3c7024603665aca39aab701ece2214050015f75c
asm <vaskocuturilo@rambler.ru>
2023-03-22 12:18:14 +0100

    DV-00002: Add moving functionality.

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,7 @@
-'use strict';
-
 const d = document.getElementsByClassName("draggable");
 
-for (let i = 0; i < document.length; i++) {
-  document[i].style.position = "relative";
+for (let i = 0; i < d.length; i++) {
+  d[i].style.position = "relative";
 }
 
 function filter(e) {
@@ -15,26 +13,31 @@ function filter(e) {
 
   target.moving = true;
 
+  //NOTICE THIS 👇 Check if Mouse events exist on users' device
   if (e.clientX) {
-    target.oldX = e.clientX; 
+    target.oldX = e.clientX; // If they exist then use Mouse input
     target.oldY = e.clientY;
   } else {
-    target.oldX = e.touches[0].clientX; 
+    target.oldX = e.touches[0].clientX; // Otherwise use touch input
     target.oldY = e.touches[0].clientY;
   }
-  
+  //NOTICE THIS 👆 Since there can be multiple touches, you need to mention which touch to look for, we are using the first touch only in this case
+
   target.oldLeft = window.getComputedStyle(target).getPropertyValue('left').split('px')[0] * 1;
   target.oldTop = window.getComputedStyle(target).getPropertyValue('top').split('px')[0] * 1;
 
   document.onmousemove = dr;
+  //NOTICE THIS 👇
   document.ontouchmove = dr;
-  
+  //NOTICE THIS 👆
+
   function dr(event) {
     event.preventDefault();
 
     if (!target.moving) {
       return;
     }
+    //NOTICE THIS 👇
     if (event.clientX) {
       target.distX = event.clientX - target.oldX;
       target.distY = event.clientY - target.oldY;
@@ -42,7 +45,8 @@ function filter(e) {
       target.distX = event.touches[0].clientX - target.oldX;
       target.distY = event.touches[0].clientY - target.oldY;
     }
-   
+    //NOTICE THIS 👆
+
     target.style.left = target.oldLeft + target.distX + "px";
     target.style.top = target.oldTop + target.distY + "px";
   }
@@ -51,14 +55,17 @@ function filter(e) {
     target.moving = false;
   }
   target.onmouseup = endDrag;
+  //NOTICE THIS 👇
   target.ontouchend = endDrag;
- 
-document.onmousedown = filter;
-document.ontouchstart = filter;
+  //NOTICE THIS 👆
 }
+document.onmousedown = filter;
+//NOTICE THIS 👇
+document.ontouchstart = filter;
+//NOTICE THIS 👆
 
 
 function myFunction(){
   document.getElementById("add_message").innerHTML = 
-  '<div class="message">Add friend<br>Name: <input type="text" id="name"> <input type="submit" id="name" value="Add friend"><br><br><br></div>';
+  '<div class="message">Add friend<br>Name: <input type="text" id="name"> <input type="submit" id="button" value="Add friend"><br><br><br></div>';
   };
